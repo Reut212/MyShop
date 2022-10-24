39c6b9f6ab9a42fbceb4c5e258ff01ad45a171a1
Reut Hadad <reuthadad123@gmail.com>
2022-10-24 23:20:32 +0300

    added pm/am format to orders list

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -25,8 +25,9 @@ class _OrderItemState extends State<OrderItem> {
         children: <Widget>[
           ListTile(
             title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm')
+                .add_jm()
+                .format(widget.order.dateTime)),
             trailing: IconButton(
               icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
               onPressed: () {
