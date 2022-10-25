bcb9720237500bd3518775409583805f6937a0a8
Reut Hadad <reuthadad123@gmail.com>
2022-10-26 02:54:00 +0300

    delete products done

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -28,7 +30,9 @@ class UserProductItem extends StatelessWidget {
               icon: Icon(Icons.edit),
               color: Theme.of(context).primaryColor),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            },
             icon: Icon(Icons.delete),
             color: Theme.of(context).errorColor,
           )
