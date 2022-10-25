d96d2afbff9a24196fbfafefecd44bf6a2d8220a
Reut Hadad <reuthadad123@gmail.com>
2022-10-25 19:07:40 +0300

    Added 'mananged products' screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routName = 'user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Your Products'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                //.
+              },
+              icon: Icon(Icons.add),
+            )
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemCount: productData.items.length,
+            itemBuilder: (_, i) => Column(
+              children: [
+                UserProductItem(
+                  productData.items[i].title,
+                  productData.items[i].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
